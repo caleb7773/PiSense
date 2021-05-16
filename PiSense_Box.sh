ec3f8a9d9fa022e069a5433977e428545e6ed862
caleb7773 <37095625+caleb7773@users.noreply.github.com>
2021-05-16 16:11:17 +0300

    Create PiSense_Box.sh

---
+++ PiSense_Box.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Enable CGI Scripts on Apache
+sudo ln -s /etc/apache2/mods-available/cgi.load /etc/apache2/mods-enabled/
+sudo systemctl restart apache2
+
+# Create CGI Script
+sudo tee -a /usr/lib/cgi-bin/third.cgi << EOF
+#!/bin/bash
+echo "Content-type: text/html"
+echo ""
+echo "<html><head><title>Bash as CGI"
+echo "</title></head><body>"
+
+echo "<h1>IP Information</h1>"
+echo "$(ifconfig | grep -A 1 eth1 | grep inet > /tmp/ip)"
+echo "$(sed -i 's/netmask.*$//g' /tmp/ip)"
+echo "$(sed -i 's/inet //g' /tmp/ip)"
+
+echo "<h2> Interface </h2>"
+echo "$(cat /tmp/int | cut -d ':' -f1)"
+echo "<br>"
+echo "<h2> IP </h2>"
+echo "$(cat /tmp/ip)"
+echo "<br>"
+echo "<br>"
+echo "<br>"
+echo "$(date)"
+echo ""
+
+echo "</body></html>"
+EOF
+
+
+
+
+
+# Give it execute rights
+sudo chmod +x /usr/lib/cgi-bin/third.cgi
+
+# Restart the apache service
+sudo systemctl restart apache2
